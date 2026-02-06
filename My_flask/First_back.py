0db4ebc77f9f6000a3b68c3340140e6e54b7f43e
Enoch <westondipo@gmail.com>
2026-02-06 19:15:39 +0000

    Add base HTML structure and styling for task manager app
    - Created a new CSS file for styling the application.
    - Added a base HTML template (Base.html) for consistent layout across pages.
    - Developed a main HTML template (Main.html) extending the base template, including a header, navigation, and task table.
    - Introduced a testing HTML template for further development and testing.
    - Added a base HTML template in a different directory for potential frontend structure.

--- My_flask/First_back.py
+++ My_flask/First_back.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 # My app
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy()
+
 @app.route("/")
+
 def index():
-    return "Testing 123"
+    return render_template("Main.html")
+
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
