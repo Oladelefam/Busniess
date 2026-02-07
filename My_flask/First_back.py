7db9d5c040467580c5a769bf931e24c469346344
Enoch <westondipo@gmail.com>
2026-02-07 16:13:00 +0000

    Implement task management features: add task creation, display tasks with creation date, and set up database model

--- My_flask/First_back.py
+++ My_flask/First_back.py
@@ -1,19 +1,54 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # My app
 
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-db = SQLAlchemy()
+db = SQLAlchemy(app)
 
-@app.route("/")
+
+#Data class - row of data
+class MyTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Content = db.Column(db.String(100), nullable=False)
+    complete = db.Column(db.Integer, default=0)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"Task (self.id)"
+
+
+#Routes to Webpage
+#Home page
+@app.route("/", methods=["POST", "GET"])
 
 def index():
-    return render_template("Main.html")
+    #Add a Task
+    if request.method == "POST":
+        Current_task = request.form['content']
+        new_task = MyTask(content=Current_task)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            print(f"Error{e}")
+            return f"Error{e}"
+    #See all current task
+    else:
+        tasks = MyTask.query.order_by(MyTask.created).all()
+
+        return render_template("Main.html", task=tasks)
 
     
 
+
+
+
 if __name__ == "__main__":
+    with app.app_context(): 
+        db.create_all()
     app.run(debug=True)
