74237f47b794422e42ba9301b3f6c6b596a2b4a6
Enoch <westondipo@gmail.com>
2026-02-20 14:39:22 +0000

    UI update

--- Finanace_tracker/template/app.py
+++ Finanace_tracker/template/app.py
@@ -4,23 +4,47 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route("/")
-
-def index():
-    return render_template("Home.html")
-
-
 #Data base
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+
 db = SQLAlchemy(app)
 
+# Finance Database
 class MyFinance(db.Model):
-    Expense = db.Column(db.String(100), nullable=False)
-    Cost = db.Column(db.Float, primary_key=True)
-    Date = db.Column(db.DateTime, default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    expense = db.Column(db.String(100), nullable=False)
+    cost = db.Column(db.Float, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
     category = db.Column(db.String(100), nullable=False)
-    Describition = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
 
+    def __repr__(self):
+        return f"Expense: {self.expense}"
+
+
+# Home Page
+@app.route("/", methods=["GET", "POST"])
+
+
+def index():
+    if request.method == "POST":
+        expense = request.form["expense"]
+        cost = request.form["cost"]
+
+        new_expense = MyFinance(
+            expense=expense,
+            cost=cost,
+            category="General",
+            description="None"
+        )
+
+        db.session.add(new_expense)
+        db.session.commit()
+        return redirect("/")
+
+    expenses = MyFinance.query.order_by(MyFinance.date).all()
+    return render_template("Home.html", expenses=expenses)
+       
  
 
 
