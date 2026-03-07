e7bca5e8ffd9ac46d93b0029fc9eeb5a7e60a825
Enoch <westondipo@gmail.com>
2026-03-07 19:12:03 +0000

    Add navigation link highlighting, balance color coding, and dark mode toggle; update layout in Main.html and styles in style.css

--- Finanace_tracker/template/static/js/script.js
+++ Finanace_tracker/template/static/js/script.js
@@ -46,4 +46,56 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-})
+});
+document.addEventListener("DOMContentLoaded", function () {
+
+     // active nav link
+
+    const navLinks = document.querySelectorAll(".nav-link");
+    const currentPage = window.location.pathname;
+
+    navLinks.forEach(link => {
+        if (currentPage.includes(link.getAttribute("href"))) {
+            link.classList.add("active-nav");
+        }
+    });
+
+    //dashboard colour balance
+
+    const balanceElement = document.querySelector(".amount");
+
+    if (balanceElement) {
+        const value = parseFloat(balanceElement.textContent.replace("$", ""));
+
+        if (!isNaN(value)) {
+            balanceElement.style.color =
+                value < 0 ? "#e53935" : "#2EC4B6";
+        }
+    }
+
+  
+
+    // pade fade in
+
+    document.body.classList.add("loaded");
+
+     // dark mode 
+
+    const toggle = document.getElementById("darkToggle");
+
+    if (toggle) {
+        toggle.addEventListener("click", function () {
+            document.body.classList.toggle("dark-mode");
+
+            localStorage.setItem(
+                "darkMode",
+                document.body.classList.contains("dark-mode")
+            );
+        });
+
+        if (localStorage.getItem("darkMode") === "true") {
+            document.body.classList.add("dark-mode");
+        }
+    }
+
+});
